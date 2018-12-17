f179f353a3db8541cc4df000cc8a461570d123b4
Andreagalera <43338979+Andreagalera@users.noreply.github.com>
2018-12-17 20:24:10 +0100

    Reccycler

--- app/src/main/java/dsa/eetac/upc/edu/preexam2followsgit/Follower.java
+++
@@ -1,8 +0,0 @@
-package dsa.eetac.upc.edu.preexam2followsgit;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Follower {
-    ArrayList<List> listadefollowers = new ArrayList();
-}
